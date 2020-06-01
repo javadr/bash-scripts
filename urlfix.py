d2d7838f66e2d6fc925153eb8166751563cc550f
javadr@gmail.com <javadr@gmail.com>
2020-06-01 07:56:28 +0430

    Jun 1, 2020

--- urlfix.py
+++ urlfix.py
@@ -1,19 +1,23 @@
-#!/bin/python
+#!/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
 import urllib.parse
 import pyperclip
+import argparse
 
-#if len(sys.argv)==1:
-def usage():
-	print ("Usage: %s <url>" % sys.argv[0][sys.argv[0].rfind('/')+1:])
-#	sys.exit(-1)
+parser = argparse.ArgumentParser(description='Process the clipboard\'s text.')
+parser.add_argument('-u', dest='urlfix_or_newline', action='store_true', )
+parser.add_argument('-n', dest='urlfix_or_newline', action='store_false', )
+args = parser.parse_args()
 
-#url = sys.argv[1]
-url = pyperclip.paste()
-#fixedurl = urllib.unquote(url)  # in python version prior to 3.6
-fixedurl = urllib.parse.unquote(url)
-print(fixedurl)
-pyperclip.copy(fixedurl)
+output = pyperclip.paste()
+
+if args.urlfix_or_newline: #urlfix
+    output = urllib.parse.unquote(output)
+#    output = urllib.unquote(output)  # in python version prior to 3.6
+else: #newline remove
+    output = output.replace('\n\n', '!$@#').replace('\n','').replace('!$@#','\n')
+
+pyperclip.copy(f"{output}")
+print(output)
 
