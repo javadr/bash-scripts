f19f598a1b37cb1060dd1b82174cbd02ab1f31b4
javad <javadr@gmail.com>
2021-05-17 17:12:18 +0430

    code sanitization

--- mw.py
+++ mw.py
@@ -1,62 +1,56 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# ***** BEGIN GPL LICENSE BLOCK *****
 #
-# Copyright (C) 2018: S.M.J.R.,
+# Copyright (C) 2018-2021: S.M.J.R.,
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-# ***** END GPL LICENCE BLOCK *****
 # Change Logs:
+# Mon 17 May 2021 02:32:55 PM +0430
+# ver 0.11: simplification of code 
 # Tue 22 May 2018 04:50:34 AM +0430
 # ver 0.01: Initial version
 
-__author__ = "S.M.J.R."
-__version__ = 0.1
-__date__ = "22May2018"
-__email__ = 'javadr@gmail.com'
+__version__ = 0.11
+__date__ = "17May2021"
 
-import sys, os
+import sys
 from itertools import permutations, combinations
-from subprocess import call
+from pathlib import Path
+
 
 def choose(chars, k):
-    for item in combinations(chars, k): makewords(item)
-
+    finalwords = set()
+    for item in combinations(chars, k):
+        finalwords.update(makewords(item))
+    return finalwords
+    
 def makewords(chars):
-    for item in permutations(chars): 
-#        count = call("grep {0} Bwords.txt -cxq".format(''.join(item)), stdout=None, shell=True)
-#        if count !=0: print(''.join(item), count)
-        w = ''.join(item)
-        if w in worddic: print(w)
-        
-        
+    words = set( ''.join(item) for item in permutations(chars) if ''.join(item) in worddic )
+    return words
+
 def usage():
-    print ("""Word Maker ver {0}[{1}]
+    print(f"""Word Maker ver {__version__} [{__date__}]
     Usage:
-    \t{2} <char1> <char2> k""".format(__version__, __date__, sys.argv[0]))
+    \t{Path(sys.argv[0]).name} <char1> <char2> .... <charN> k  \t # k <= N""")
 
 if __name__ == '__main__':
-    chars = sys.argv[1:-1] if len(sys.argv) > 4 else sys.stdin.readlines()
-    k = int(sys.argv[-1])
-    if not chars:#len(sys.argv) < 2 and len(linesinput)==0:
+    chars = sys.argv[1:-1]
+    try: 
+        k = int(sys.argv[-1])
+    except ValueError: 
         usage()
         sys.exit()
-        
-    curdir = os.path.dirname(os.path.realpath(__file__)) 
-    worddic = {i:1 for i in open("{0}/Bwords.txt".format(curdir)).read().split()}        
-#    print(worddic)
 
-    choose(chars, k)        
+    if not chars:  #len(sys.argv) < 2
+        usage()
+        sys.exit()
+    
+    # os.path.dirname(os.path.realpath(__file__))
+    curdir = Path(sys.argv[0]).parent.absolute()
+    worddic = {i: 1 for i in open(f"{curdir}/Bwords.txt").read().split()}
+
+    print('\n'.join(choose(chars, k)))
