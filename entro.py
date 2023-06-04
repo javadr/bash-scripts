2ee59ae7033fc677f9ca9fd2583e01e51e9e2809
javad <javadr@gmail.com>
2023-06-04 08:43:49 +0330

    (feat) supports just one prob (p0) input and makes the other (1-p0)

--- entro.py
+++ entro.py
@@ -1,12 +1,14 @@
-#!/bin/python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 import sys
 from math import log
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 2:
     print("Usage: %s <n1> <n2> ..." % sys.argv[0][sys.argv[0].rfind('/') + 1:])
     sys.exit(-1)
+
+if len(sys.argv) == 2:
+    sys.argv.append(f"{1-eval(sys.argv[1])}")
 
 entropy = 0.0
 isum = 0
