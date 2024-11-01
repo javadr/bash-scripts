c00334ed8f69cc28a237aa8f070b8c3e5988f97f
javad <javadr@gmail.com>
2024-11-01 13:29:21 +0100

    (style) entro.py

--- entro.py
+++ entro.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
+# ruff: noqa: T201
 
 import sys
 from math import log
 
 if len(sys.argv) < 2:
-    print("Usage: %s <n1> <n2> ..." % sys.argv[0][sys.argv[0].rfind('/') + 1:])
+    print(f"Usage: {sys.argv[0][sys.argv[0].rfind('/') + 1:]} <n1> <n2> ...")
     sys.exit(-1)
 
 if len(sys.argv) == 2:
     # sys.argv.append(f"{1-eval(sys.argv[1])}")
-    if sys.argv[1].find("/")>0:
+    if sys.argv[1].find("/") > 0:
         num, den = map(float, sys.argv[1].split("/"))
         sys.argv.append(f"{den-num:g}/{den:g}")
     else:
-        sys.argv[1] = f"{eval(sys.argv[1]):g}"    
+        sys.argv[1] = f"{eval(sys.argv[1]):g}"
         sys.argv.append(f"{1-eval(sys.argv[1])}")
-        
 
 
 print(f"Probabilities include {', '.join(sys.argv[1:])}")
 
 entropy = 0.0
-isum = 0
+summation = 0
 for item in sys.argv[1:]:
     ni = eval(item)
-    if ni != 0: entropy += -ni * log(ni, 2)
-    isum += ni
-entropy = entropy / isum + log(isum, 2)
+    if ni != 0:
+        entropy += -ni * log(ni, 2)
+    summation += ni
+entropy = entropy / summation + log(summation, 2)
 
-print("entropy is: {}".format(entropy))
+print(f"entropy is: {entropy}")
